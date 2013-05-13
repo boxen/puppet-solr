2a88008b10484671433ef9d421a17966ddb918ef
Nick Novitski <nicknovitski@gmail.com>
2013-05-13 15:22:44 -0600

    update formula for v4.3.0

--- files/brews/solr.rb
+++ files/brews/solr.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Solr < Formula
   homepage 'http://lucene.apache.org/solr/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.1.0/solr-4.1.0.tgz'
-  sha1 '89cce8c7c8d68df20f009a7c04ede423d6fa7c84'
+  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.3.0/solr-4.3.0.tgz'
+  sha1 '8c62319d7eb0a42a223888d98aa9f5e1472bd7af'
 
-  version '4.1.0-boxen1'
+  version '4.3.0-boxen1'
 
   def script; <<-EOS.undent
     #!/bin/sh
